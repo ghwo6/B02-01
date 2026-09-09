fe8b6421ac873e73bd4dade1e628a13f43efa5e4
Hojae <ghwo6@naver.com>
2026-09-09 14:47:29 +0900

    ADD:기능 구현중

--- dataclass.py
+++ dataclass.py
@@ -5,14 +5,14 @@ from typing import Optional
 class Caterogies:
     name : str
 
-# 카테고리 연동 안됨
+# 카테고리 연동 안됨 -> Categories로 설정 해놨는데 적절했는지 궁금함
 @dataclass
 class Transactions:
     id : str
     type : str
     date : str
     amount : int
-    category : str
+    category : Caterogies
     memo : Optional[str] = None
     tags : Optional[str] = None
 
--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import sys
 
+PROGRAM_END_POINT = "./main.py"
+USAGE_LIST = ["add","list","update","delete","search","summary","budget","category","import","export"]
 
-def parse_args():
+def print_defalut_use():
+    ...
+
+def print_usage():
+    print("\n",*USAGE_LIST[0:len(USAGE_LIST)//2])
+    print(*USAGE_LIST[len(USAGE_LIST)//2:],"\n")
+
+    return
+
+def route(func,args:list):
+    ...
+
+# 일단은 매개변수가 있으면 출력하는 기능으로 구현함
+def parser():
     if len(sys.argv) >1 :
+        if sys.argv[1] == "--h":
+
+            print("\n",f"python {PROGRAM_END_POINT} usage")
+            print("\n","usage는 다음과 같습니다.")
+            print_usage()
+
+            # print("add , list , update , delete , search")
+            # print("summary , budget , category , import , export")
+
+            
+        else:
+            # 라우팅 하는 부분
+            if sys.argv[1] in USAGE_LIST:
+                print("ok")
+            else:
+                print("사용 방법을 확인해주세요.")
+
+
+        # 실습을 위해 출력해봄
         for v in sys.argv[1:]:
             print(v)
+    else:
+        print("arg가 입력되지 않았습니다.")
+        print("사용법이 필요하시면 --help를 입력해주세요.")
 
 if __name__ == "__main__":
-    parse_args()
+    parser()
