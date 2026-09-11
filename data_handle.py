1208b2b94b750e37c0f22ad0dd6a3f86c4577c98
Hojae <ghwo6@naver.com>
2026-09-11 22:06:25 +0900

    ADD:260911작성 코드

--- data_handle.py
+++ data_handle.py
@@ -1,11 +1,30 @@
 import os,json
+from dataclass import init_categories_file
 
 DIR_NAME = os.path.dirname(__file__)
 TRANSACTIONS_FILE = os.path.join(DIR_NAME,"transactions.jsonl")
 CATEGORIES_FILE = os.path.join(DIR_NAME,"categories.jsonl")
 BUDGETS_FILE = os.path.join(DIR_NAME,"budgets.jsonl")
 
+def default_file_check():
+    if os.path.exists(TRANSACTIONS_FILE):
+        print("transactions.jsonl 파일 로드됨")
+    else:
+        make_like_touch(TRANSACTIONS_FILE)
 
+    if os.path.exists(CATEGORIES_FILE):
+        print("categories.jsonl 파일 로드됨")
+    else:
+        init_categories_file(CATEGORIES_FILE)
+    if os.path.exists(BUDGETS_FILE):
+        print("budgets.jsonl 파일 로드됨")
+    else:
+        make_like_touch(BUDGETS_FILE)
+
+def make_like_touch(filepath:str):
+    with open(filepath,'wt',encoding="utf-8"):
+        return
+    
 
 # def load_json(file_name:str):
 #     try:
@@ -14,3 +33,6 @@ BUDGETS_FILE = os.path.join(DIR_NAME,"budgets.jsonl")
 #     except FileNotFoundError as e:
 #         # 초기 파일 만들기
 #         ...
+
+if __name__ =="__main__":
+    default_file_check()
--- dataclass.py
+++ dataclass.py
@@ -1,25 +1,59 @@
-from dataclasses import dataclass
+from dataclasses import dataclass,asdict
 from typing import Optional
+import json
 
 @dataclass
-class Caterogies:
+class Category:
+    type : str
     name : str
 
-# 카테고리 연동 안됨 -> Categories로 설정 해놨는데 적절했는지 궁금함
+
+DEFAULT_CATEGORIES : list[Category] = [
+    # 지출
+    Category("expense","식비"),
+    Category("expense","교통"),
+    Category("expense","주거"),
+    Category("expense","통신"),
+    Category("expense","쇼핑"),
+    Category("expense","여가"),
+    Category("expense","의료"),
+    Category("expense","기타"),
+    # 수입
+    Category("income","급여"),
+    Category("income","부수입"),
+    Category("income","금융수입"),
+    Category("income","기타수입")
+]
+
+def init_categories_file(filepath:str):
+    with open(filepath,'wt',encoding="utf-8") as f:
+        for cat in DEFAULT_CATEGORIES:
+            # astype(cat) 를 이용해서 파일에 jsonl형식으로 저장하자.
+            # ensure_ascii = False 로 해야 한글이 안깨진다고 함 
+            line = json.dumps(asdict(cat),ensure_ascii=False)
+            f.write(line + "\n")
+
+# 카테고리 연동 안됨 -> Categories로 설정 해놨는데 적절했는지 궁금함 -> str 로 변경
+# category: Category -> category:str
+# 카테고리를 생성할때와는 다르게
+# 서비스를 이용하는 고객이 search로 카테고리를 선정해 검색해 볼때는 중첩 구조가 복잡해지게됨
 @dataclass
-class Transactions:
+class Transaction:
     id : str
-    type : str
-    date : str
+    type : str              ## income | expense
+    date : str              ## 'YYYY-MM-DD'
     amount : int
-    category : Caterogies
+    category : str          ## 카테고리 이름(참조 키만 보관)
     memo : Optional[str] = None
-    tags : Optional[str] = None
+    tags : Optional[list[str]] = None
 
-
+# tags는 여러개가 들어갈수 있으니 str보다는 list[str]이 더 낫다고 함 Gemini
 
 @dataclass
-class Budgets:
+class Budget:
     ym : str
     budget : int
 
+
+if __name__ =="__main__":
+    ...
