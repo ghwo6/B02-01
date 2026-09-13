eb588bb17412a824b54d0507d7e505543ab1b4c6
Hojae <ghwo6@naver.com>
2026-09-13 21:11:19 +0900

    ADD:parser학습 으로 내용 추가함

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 PROGRAM_END_POINT = "./main.py"
 USAGE_LIST = ["add","list","update","delete","search","summary","budget","category","import","export"]
@@ -17,11 +18,35 @@ def print_usage():
 
     return
 
-def route(func,args:list):
+def f_add():
     ...
 
 # 일단은 매개변수가 있으면 출력하는 기능으로 구현함
 def parser():
+
+    parser = argparse.ArgumentParser(prog="budget_app")
+
+    # 사용자가 입력한 명령어 이름을 'command'라는 변수에 담도록 설정 with GEMINI
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # 각 명령어를 독립된 서브 커맨드로 입력함 with GEMINI
+
+    # ["add","list","update","delete","search","summary","budget","category","import","export"]
+    p_add = subparsers.add_parser("add",help="거래 추가")
+    p_add.set_defaults(func=f_add)
+
+    p_list = subparsers.add_parser("list",help="거래 추가")
+
+    p_update = subparsers.add_parser("update",help="거래 추가")
+    p_delete = subparsers.add_parser("delete",help="거래 추가")
+    p_search = subparsers.add_parser("search",help="거래 추가")
+    p_summary = subparsers.add_parser("summary",help="거래 추가")
+    p_budget = subparsers.add_parser("budget",help="거래 추가")
+    p_category = subparsers.add_parser("category",help="거래 추가")
+    p_import = subparsers.add_parser("import",help="거래 추가")
+    p_export = subparsers.add_parser("export",help="거래 추가")
+
+
     if len(sys.argv) >1 :
         if sys.argv[1] == "--h":
 
