265b249a9a7d108db4b228381b13bfc28137caa6
김호재 <ghwo61351@c6r3s6.codyssey.kr>
2026-09-17 21:40:25 +0900

    ADD: 날짜 입력함수 추가함

--- user_input.py
+++ user_input.py
@@ -4,12 +4,12 @@ def date_input():
     # 잘못 되면 다시 실행 하기 위해 while문
     while True:
         # 오류날 수 있으니 0으로 초기화함 (y , m , d)
-        y , m , d = 0
+        y , m , d = 0,0,0
         # 가공할수 있으나 헷갈리므로 raw_input으로 설정함
-        raw_input = input("날짜를 입력해 주세요.").strip()
+        raw_input = input("날짜를 입력해 주세요.").strip().split()
         if len(raw_input) == 3:
             # 3개가 입력됬을떄
-            y , m , d = raw_input[0:2]
+            y , m , d = raw_input
         else:
             # 3개가 아닐때
             print("년 월 일 ex) 2026 09 15 와 같이 '띄어쓰기'로 구분하여 주십니다.")
@@ -21,10 +21,37 @@ def date_input():
             print("숫자가 아닌 값을 입력하셨습니다.")
             print("다시 입력해 주세요.")
 
+def date_input_ref():
+    # 잘못 되면 다시 실행 하기 위해 while문
+    while True:
+        # 오류날 수 있으니 0으로 초기화함 (y , m , d)
+        y , m , d = 0,0,0
+        # 가공할수 있으나 헷갈리므로 raw_input으로 설정함
+        raw_input = input("날짜를 입력해 주세요.").strip().split()
+        if len(raw_input) == 3:
+            # 3개가 입력됬을떄
+            y , m , d = raw_input
+        else:
+            # 3개가 아닐때
+            print("년 월 일 ex) 2026 09 15 와 같이 '띄어쓰기'로 구분하여 주십니다.")
+            continue
+        if y=="0" or m =="0" or d == "0":
+            print("0을 입력하셨습니다. 정확하게 년 월 일을 입력해주세요")
+            continue
+        try:
+            datetime.datetime.strptime(f"{y} {m} {d}", "%Y %m %d")
+        except ValueError:
+            print("날짜를 잘못 입력하셨습니다.")
+            continue
+        
+        return f"{y} {m} {d}"
+
 def validate_date(date_text):
     try:
-        datetime.datetime.strptime(date_text,"%Y-%m-%d")
-
+        date = datetime.datetime.strptime(date_text,"%Y-%m-%d")
+        return date
+    except ValueError:
+        print("날짜를 잘못 입력하셨습니다.")
+    
 if __name__ == "__main__":
-
-    ...
+    print(date_input_ref())
